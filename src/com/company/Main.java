9a826ea957c7c434bdb3f4378596763735f248d9
yashodgayashan <yashodgayashan@gmail.com>
2020-02-18 12:29:33 +0530

    Added read file class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+            // TODO Auto-generated method stub
+            BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
+            String jsonStr = "";
+            try {
+                StringBuilder sb = new StringBuilder();
+                String line = reader.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                    line = reader.readLine();
+                }
+                jsonStr = sb.toString();
+            } finally {
+                reader.close();
+            }
+            System.out.print(jsonStr);
+        }
     }
-}
+
