838d8fe43bc3d1855b6632ab18483a15f6229ad8
yashodgayashan <yashodgayashan@gmail.com>
2020-02-18 14:32:19 +0530

    Added formatting json.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 import org.json.*;
 
 
@@ -33,33 +35,55 @@ public class Main {
             for (int i = 0; i < jsonArr.length(); i++)
             {
                 JSONObject jsonObj = jsonArr.getJSONObject(i);
+                String name = jsonObj.getString("name");
+                String endpoint = jsonObj.getString("endpoint");
+                JSONArray parameters;
+                try{
+                    parameters = jsonObj.getJSONArray("parameters");
+                    for (int j = 0; j < parameters.length();j++){
+                        JSONObject jsonObjTwo = parameters.getJSONObject(j);
+                        Iterator<String> keys = jsonObjTwo.keys();
+                        while (keys.hasNext()) {
+                            String key = keys.next();
+                            System.out.print(key + " ");
+                            System.out.println(jsonObjTwo.getString(key));
+                        }
+                    }
+                }catch (Exception e){
 
-                System.out.println(jsonObj);
+                }
+
+                System.out.println(endpoint+"/"+name+"?");
+                System.out.println();
+//                System.out.println(name);
+//                System.out.println(jsonObj);
             }
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        // Windows
-        processBuilder.command("sqlmap", "-u", "http://www.bible-history.com/subcat.php?id=2","--dbs");
 
-        try {
 
-            Process process = processBuilder.start();
-
-            BufferedReader readerTwo =
-                    new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = readerTwo.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("\nExited with error code : " + exitCode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//
+//        processBuilder.command("sqlmap", "-u", "http://www.bible-history.com/subcat.php?id=2","--dbs");
+//
+//        try {
+//
+//            Process process = processBuilder.start();
+//
+//            BufferedReader readerTwo =
+//                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+//
+//            String line;
+//            while ((line = readerTwo.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//            int exitCode = process.waitFor();
+//            System.out.println("\nExited with error code : " + exitCode);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         }
     }
