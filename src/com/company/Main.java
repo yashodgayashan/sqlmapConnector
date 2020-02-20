d82f4eade2022093c3985c020bb6f916beba93ce
yashodgayashan <yashodgayashan@gmail.com>
2020-02-20 09:28:05 +0530

    Developed getFileContect function.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Iterator;
 import org.json.*;
 
@@ -12,23 +9,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-            // TODO Auto-generated method stub
-            BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
-            String jsonStr = "";
-            try {
-                StringBuilder sb = new StringBuilder();
-                String line = reader.readLine();
+           String fileData = getFileContent("text.txt");
 
-                while (line != null) {
-                    sb.append(line);
-                    sb.append("\n");
-                    line = reader.readLine();
-                }
-                jsonStr = sb.toString();
-            } finally {
-                reader.close();
-            }
-            JSONArray jsonArr = new JSONArray(jsonStr);
+            JSONArray jsonArr = new JSONArray(fileData);
 
             for (int i = 0; i < jsonArr.length(); i++)
             {
@@ -53,36 +36,56 @@ public class Main {
 
                 System.out.println(endpoint+"/"+name+"?");
                 System.out.println();
-//                System.out.println(name);
-//                System.out.println(jsonObj);
+//
             }
+//
+//
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//
+//        processBuilder.command("sqlmap", "-u", "http://www.bible-history.com/subcat.php?id=2","--dbs");
+//
+//        try {
+//
+//            Process process = processBuilder.start();
+//
+//            BufferedReader readerTwo =
+//                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+//
+//            String line;
+//            while ((line = readerTwo.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//            int exitCode = process.waitFor();
+//            System.out.println("\nExited with error code : " + exitCode);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-
-        ProcessBuilder processBuilder = new ProcessBuilder();
-
-        processBuilder.command("sqlmap", "-u", "http://www.bible-history.com/subcat.php?id=2","--dbs");
-
-        try {
-
-            Process process = processBuilder.start();
-
-            BufferedReader readerTwo =
-                    new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = readerTwo.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("\nExited with error code : " + exitCode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
 
+        public static String getFileContent(String fileName) throws IOException {
+
+            BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
+            String data = "";
+            try {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = reader.readLine();
+
+                while (line != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
+                    line = reader.readLine();
+                }
+                data = stringBuilder.toString();
+            } finally {
+                reader.close();
+            }
+            return data;
         }
+
     }
 
